1775c6e0a69038c4ab604c5694e7fa2ad6ea85a4
TimL94 <99834249+TimL94@users.noreply.github.com>
2024-04-18 12:58:44 -0400

    started working on routes

--- public/assets/js/routes/index.js
+++ public/assets/js/routes/index.js
@@ -1,1 +1,21 @@
 const router = require('express').Router();
+const { error } = require('console');
+const fs = require('fs');
+const path =require('path');
+
+const dbFilePath = path.resolve(__dirname, '../db.json');
+
+router.get('/notes', (req, res) => {
+    fs.readFile(dbFilePath, 'utf8', (error, data) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error'});
+            return;
+        }
+
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
+
+
