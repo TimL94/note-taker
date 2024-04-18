a1dd1118711c319da1504fe35b9b5b27bb592342
TimL94 <99834249+TimL94@users.noreply.github.com>
2024-04-18 12:53:45 -0400

    intitial code upload

---
+++ public/assets/js/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const api = require ('./routes/index');
+const { getAndRenderNotes } = require('./index');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+});
